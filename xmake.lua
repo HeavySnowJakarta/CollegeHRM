944a532ddac0bc3afdbdea1842b3447bf5d1556c
Heavysnowjakarta <heavysnowjakarta@gmail.com>
2024-06-17 15:07:36 +0800

    upload xmake issue ref; ignore .vscode; try to fix compile order

--- xmake.lua
+++ xmake.lua
@@ -1,20 +1,37 @@
+-- Project name and language.
 set_project("CollegeHRM")
 set_languages("c++20")
 
+-- Basic rules, including generating `compile_command.json` automatically.
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = '.vscode'})
+set_config("buildir", "build")
+set_config("export_compile_commands", true)
+
+-- Define a rule to ensure modules are compiled and put into right
+-- directories.
+rule("cpp20module")
+    set_extensions(".ixx")
+    on_build_file(function(target, sourcefile)
+        import("core.project.depend")
+        import("core.tool.compiler")
+        local dependinfo = depend.load(target, sourcefile, {include = true})
+        local compinst = compiler.load("cxx", {target = target})
+        local objectfile = path.join(
+            target:autogendir(),
+            "rules",
+            "modules",
+            path.basename(sourcefile) .. ".obj"
+        )
+        depend.save(target, objectfile, dependinfo)
+        os.vexecv(compinst:compcmd({sourcefile, "-Fo" .. objectfile, "-c"}))
+    end)
 
 -- The progress of the compiling of the backend.
 target("backend")
     set_kind("static")
-
-    add_files("src/lib/*.ixx")
-    add_includedirs("src/lib")
-
-    -- Ensure the order to compile.
-    before_build(function (target)
-        os.exec("xmake build src/lib/employee.ixx")
-    end)
+    add_files("src/lib/*.ixx", {rules = "cpp20module"})
+    add_includedirs("$(buildir)/$(os)/$(arch)/$(mode)")
 
 -- The main progress.
 target("CollegeHRM")
